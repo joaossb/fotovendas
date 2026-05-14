14bc780dd0a92f7c080f98be38e81aa3cb718a96
Joao Baiao <joao.baiao@hotmail.com>
2026-05-14 11:06:30 -0300

    reset senha em componente separado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ResetPassword from "./ResetPassword.jsx";
 
-createRoot(document.getElementById('root')).render(
+const isReset = window.location.hash.includes("type=recovery") ||
+                window.location.hash.includes("error_code=otp_expired") ||
+                window.location.hash.includes("error=access_denied");
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {isReset ? <ResetPassword /> : <App />}
+  </StrictMode>
+);
